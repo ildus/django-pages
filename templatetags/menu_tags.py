35d1e110a3066369be4bd98d40a05782ca46d6c5
Andrey Grygoryev <grandse@smyt.ru>
2012-08-19 15:19:51 +0000

    Fix page ordering

--- templatetags/menu_tags.py
+++ templatetags/menu_tags.py
@@ -12,12 +12,6 @@ from .. import models
 register = template.Library()
 
 
-def get_cmp(pages):
-    def cmp_func(first, second):
-        return pages.index(first.id) > pages.index(second.id)
-    return cmp_func
-
-
 @register.simple_tag(takes_context=True)
 def menu_items(context, var_name, alias):
     '''Put a list of menu items related to menu with specified alias into
@@ -27,10 +21,11 @@ def menu_items(context, var_name, alias):
     pages = models.MenuItem.objects.filter(menu=menu).values_list('page_id',
                                                 flat=True).order_by('order')
     page_ids = [page_id for page_id in pages]
+    order = {page_id: index for index, page_id in enumerate(page_ids)}
     language = models.Language.objects.filter(code=settings.LANGUAGE_CODE)
     translations = models.PageTranslation.objects.filter(language=language,
                                                          page_id__in=pages)
-    context[var_name] = sorted(translations, cmp=get_cmp(page_ids))
-    print context[var_name]
+    translations_map = {order[transl.id]: transl
+                        for transl in translations}
+    context[var_name] = [translations_map[index] for index in range(len(page_ids))]
     return ''
-
