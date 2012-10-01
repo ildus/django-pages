f9e8609c4d76754056be10bef7fbe9b255828a35
Andrey Grygoryev <undeadgrandse@gmail.com>
2012-10-01 20:59:58 +0300

    Add setup script and license.

---
+++ setup.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+try:
+    from setuptools import setup
+except ImportError:
+    from ez_setup import use_setuptools
+    use_setuptools()
+    from setuptools import setup
+
+setup(
+    name='django-pages',
+    version='0.1.0-alpha',
+    description='A flexible pages application for django for future extending',
+    author='Andrey Grygoryev',
+    author_email='undeadgrandse@gmail.com',
+    url='https://github.com/GrAndSE/django-pages',
+    long_description=open('README', 'r').read(),
+    packages=('pages', ),
+    package_data={'pages': ('templates/admin/*', ), },
+    zip_safe=False,
+    requires=(),
+    classifiers=(
+        'Development Status :: 4 - Beta',
+        'Environment :: Web Environment',
+        'Framework :: Django',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+    ),
+)
